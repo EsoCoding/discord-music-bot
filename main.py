39edfb248a9b5a3e02efa2e36f339ae5a653c248
EsoCode <arjanpoortman@gmail.com>
2023-10-30 23:30:30 +0100

    feat: Add new feature

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 # This class represents the MusicBot
+import os
 from dotenv import load_dotenv
 from bot.bot import Bot
 from logging import Logger
+import logging
 
 load_dotenv()
 
 logging = Logger()
+logging.basicConfig(level=logging.INFO)
 
-if __name__ == "__main__":
+
+def main():
     bot = Bot()
     bot.start()
+
+
+if __name__ == "__main__":
+    main()
